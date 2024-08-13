ae9282dacea862e1ed93e244a4af901a2bb134fe
J0hyun <bboreber04@gmil.com>
2024-08-13 15:57:03 +0900

    first commit

---
+++ HelloJava/src/sw/mbccom/kr/Hello.java
@@ -0,0 +1,19 @@
+package sw.mbccom.kr;
+
+public class Hello {
+	public static void main(String[] args) { // 시작점
+		System.out.println("Hello Java!");
+		// 기존 자료형
+		// 정수형 byte(1byte), short(2byte), int(4byte), long(8byte)
+		// 실수형 float(4byte), double (8byte) 
+		// 논리형 boolean (bit)
+		// 문자 char(2byte), 
+		// 참조 자료형
+		
+	int x = 10;
+	boolean result = (x >= 20);
+	System.out.println(result);
+		
+	}
+  
+}
---
+++ Java20240813/src/ex04/캐스트형변환.java
@@ -0,0 +1,31 @@
+package ex04;
+
+public class 캐스트형변환 {
+
+	public static void main(String[] args) {
+		// 수동 형변환 or 강제 형변환
+		// 큰 자료형 -> 작은 자료형 대입한다.
+		short s1 = 32767;
+		int num1 = 50000;
+		
+		s1 = (short)num1; // 강제 형변환 int -> short -> 50000
+		
+		System.out.println(s1);
+		
+		double d1 = 10.2;
+		
+		int num2 = (int)d1; // 강제 형변환
+		
+		System.out.println(num2);
+		
+		System.out.println("----------------------");
+		
+		double d2 = 10 + 10.2;
+		
+		String str1 = 10 + "20";
+		System.out.println(d2);
+		System.out.println(str1);
+
+	}
+
+}
